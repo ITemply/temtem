82cd34a40e95fea2b47938a52dd272eb2c66b13c
I Temply <bluerambles278@gmail.com>
2024-01-12 18:29:05 +0000

    Updated Pages, Looks Better
    Signed-off-by: I Temply <bluerambles278@gmail.com>

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('node:path'); 
-const app = express();
+const express = require('express')
+const path = require('node:path')
+const app = express()
 
-app.set('views', path.join(__dirname, 'public'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 app.get('/signin', (req, res) => {
-  res.render('signin');
-});
+  res.render('signin')
+})
 
 app.get('/signup', (req, res) => {
-  res.render('signup');
-});
+  res.render('signup')
+})
 
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
-});
+  console.log('Server started on http://localhost:3000')
+})
